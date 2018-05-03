8ac81f81cef510e115b1aa1c9ff820c51946aaef
JoséGuilherme <guislima47@gmail.com>
2018-05-02 22:12:22 -0300

    [ Desafio 3] - Corrections

--- desafio_front_shipp/desafio.js
+++ desafio_front_shipp/desafio.js
@@ -2,16 +2,27 @@
     "use strict";
     //resolucao aqui
 
-    let totalNodes= 0;
-
-
+    let idsThings = 0;
 
     const controler = () => {
-      const botao = document.getElementsByClassName('action-add');
-      SEA.addEventListener(botao[0],"click", () => { createNode() })
+      removeAll();
+
+      const adicionar = (document.getElementsByClassName('action-add'))[0];
+      SEA.addEventListener(adicionar,"click", () => { createNode() })
+
+      const remover = (document.getElementsByClassName('action-remove'))[0];
+      SEA.addEventListener(remover,"click", () => {removeAll()})
 
     }
 
+    const removeAll = () =>{
+      const pool = document.getElementById('pool');
+
+      while (pool.firstChild){
+        pool.removeChild(pool.firstChild)
+      }
+
+    }
 
     const randomColor = () => {
         const hexadecimais = '0123456789ABCDEF';
@@ -23,16 +34,50 @@
     }
 
     const createNode = () =>{
-      const pool= document.getElementById('pool');
 
 
-      let thing = document.createElement('div');
-      thing.setAttribute('class','thing')
-      thing.setAttribute('style', 'background-color: '+ randomColor())
-      pool.appendChild(thing);
-      totalNodes++
-      document.getElementById('info').innerHTML = "A Piscina Contém " + totalNodes + " Coisas."
-      console.log(totalNodes  )
+        const pool= document.getElementById('pool');
+        let counter = pool.childElementCount + 1
+        let thing = document.createElement('div');
+
+        thing.setAttribute('class','thing');
+        thing.style.background = randomColor();
+        thing.setAttribute('id', idsThings);
+
+        let color = (document.createElement('button'))
+        color.setAttribute('class','action-change-color');
+        color.setAttribute('id',idsThings+'color')
+
+        SEA.addEventListener(color,"click", (e) => {
+          let thing = (document.getElementById(e.toElement.attributes.id.value)).parentNode;
+          thing.style.background = randomColor();
+         } )
+
+        let remove = (document.createElement('a'));
+
+        remove.setAttribute('class','action-remove');
+        remove.setAttribute('id', idsThings+"remove");
+        remove.setAttribute('href','#')
+
+        SEA.addEventListener(remove, "click", (e) =>{
+          e.preventDefault();
+          let thing = (document.getElementById(e.toElement.attributes.id.value)).parentNode;
+          thing.parentNode.removeChild(thing)
+
+        })
+
+        remove.appendChild(document.createTextNode('remover'));
+
+        color.appendChild(document.createTextNode('color'));
+        thing.appendChild(color);
+
+        thing.appendChild(remove)
+        pool.appendChild(thing);
+
+        idsThings ++;
+
+        document.getElementById('info').innerHTML = "A Piscina Contém " + counter + " Coisas.";
+
     }
 
     controler();
